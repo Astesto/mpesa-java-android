dd477a021cf2dde509b68a3c561773cc42b01c21
Astesto <astestoabu@gmail.com>
2020-08-14 09:49:39 +0300

    Initial commit

---
+++ app/src/main/java/com/abubakar/mtenantmpesa/Services/STKPushService.java
@@ -0,0 +1,18 @@
+package com.abubakar.mtenantmpesa.Services;
+import com.abubakar.mtenantmpesa.Model.AccessToken;
+import com.abubakar.mtenantmpesa.Model.STKPush;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface STKPushService {
+    @POST("mpesa/stkpush/v1/processrequest")
+    Call<STKPush> sendPush(@Body STKPush stkPush);
+
+    @GET("oauth/v1/generate?grant_type=client_credentials")
+    Call<AccessToken> getAccessToken();
+
+
+}
